207816eb06e4c95e64260c4cfdb93806af476eb1
Nina Doschek <ndoschek@eclipsesource.com>
2023-04-07 14:55:48 +0200

    #45 Update ESlint config to use dynamic year in headers
    Resolves #45

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,38 @@
 /** @type {import('eslint').Linter.Config} */
+const year = new Date().getFullYear();
 module.exports = {
     root: true,
-    extends: [
-        './configs/base.eslintrc.json',
-        './configs/warnings.eslintrc.json',
-        './configs/errors.eslintrc.json'
-    ],
-    ignorePatterns: [
-        '**/{node_modules,lib}'
-    ],
+    extends: ['./configs/base.eslintrc.json', './configs/warnings.eslintrc.json', './configs/errors.eslintrc.json'],
+    ignorePatterns: ['**/{node_modules,lib}'],
     parserOptions: {
         tsconfigRootDir: __dirname,
         project: 'tsconfig.json'
-    }
+    },
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            rules: {
+                // eslint-plugin-header
+                'header/header': [
+                    2,
+                    'block',
+                    [
+                        {
+                            pattern: '[\n\r]+ \\* Copyright \\([cC]\\) \\d{4}(-\\d{4})? .*[\n\r]+',
+                            template: `********************************************************************************
+  * Copyright (c) ${year} EclipseSource and others.
+  *
+  * This program and the accompanying materials are made available under the
+  * terms of the Eclipse Public License v. 2.0 which is available at
+  * https://www.eclipse.org/legal/epl-2.0, or the MIT License which is
+  * available at https://opensource.org/licenses/MIT.
+  *
+  * SPDX-License-Identifier: EPL-2.0 OR MIT
+  ********************************************************************************`
+                        }
+                    ]
+                ]
+            }
+        }
+    ]
 };
